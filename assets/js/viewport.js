cc02f97b6442cc388d4b02d26e3da2ff0885bd28
ntrix <ntrix@ntien.com>
2020-12-03 17:55:28 +0100

    style and cleanup

--- assets/js/viewport.js
+++ assets/js/viewport.js
@@ -41,9 +41,6 @@ window.addEventListener("scroll", setViewportVisibility);
 function elementInViewport(e) {
     const bounding = e.getBoundingClientRect();
     const myElementHeight = e.offsetHeight;
-    // debugger;
-    if(e.className.search('timeline-inverted') > -1)
-    console.log(e, bounding, myElementHeight)
     return (bounding.top >= -myElementHeight
         && bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) + myElementHeight);
 }
